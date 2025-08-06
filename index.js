c37ec28e536078fe9b935b51416c5171322ed3bd
Raja Sai Datha Sushanth <rajasaidathasushanth2006@gmail.com>
2025-08-06 23:24:45 +0530

    trying to deploy

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const VenderRouter = require('./routes/VendorRouter')
-const FirmRouter = require('./routes/FirmRouter')
-const ProductRouter = require('./routes/ProductRouter')
+const VenderRouter = require('./routes/VendorRouter');
+const FirmRouter = require('./routes/FirmRouter');
+const ProductRouter = require('./routes/ProductRouter');
 const app = express();
-const cors = require('cors')
-const path = require('path')
+const cors = require('cors'); // Only require once
+const path = require('path');
 
 app.use(express.json());
-const cors = require('cors');
 
 const allowedOrigins = [
   'http://localhost:5173',
@@ -17,7 +16,8 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-  origin: function(origin, callback) {
+  origin: function (origin, callback) {
+    // If no origin (e.g., for Postman or server-side requests), allow it
     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
@@ -26,29 +26,29 @@ app.use(cors({
   }
 }));
 
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.use((req, res, next)=>{
-    console.log(req.method, req.path)
-    next()
-})
+app.use((req, res, next) => {
+  console.log(req.method, req.path);
+  next();
+});
 
-app.use("/vendor",VenderRouter);
-app.use("/firm",FirmRouter);
-app.use("/product",ProductRouter);
-app.use("/uploads",express.static('uploads'))
-app.use("/",(req, res)=>{
-    res.send("<h1 align='center'>Welcome to HungerStop</h1>")
-})
+app.use("/vendor", VenderRouter);
+app.use("/firm", FirmRouter);
+app.use("/product", ProductRouter);
+app.use("/uploads", express.static('uploads'));
 
+app.use("/", (req, res) => {
+  res.send("<h1 align='center'>Welcome to HungerStop</h1>");
+});
 
 mongoose.connect(process.env.MONGO_URI)
-.then(()=>{
-    app.listen(process.env.PORT,()=>{
-        console.log(`Server started and Listening at Port ${process.env.PORT}`)    
-        console.log("Connected to database")
-    })
-})
-.catch((error)=>{
-    console.log(error)
-})
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`Server started and listening at port ${process.env.PORT}`);
+      console.log("Connected to database");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
