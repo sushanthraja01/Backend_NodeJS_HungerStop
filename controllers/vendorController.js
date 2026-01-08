a1f76bbee20188a6efca4a30ee4092dc80046cbd
sushanthraja01 <rajasaidathasushanth2006@gmail.com>
2026-01-08 15:56:25 +0530

    Fix environment variable case for cloud_name

--- controllers/vendorController.js
+++ controllers/vendorController.js
@@ -12,7 +12,7 @@ require('dotenv').config();
 require('../middleware/passport')
 
 cloudinary.config({
-    cloud_name : process.env.cloud_name,
+    cloud_name : process.env.CLOUD_NAME,
     api_key : process.env.api_key,
     api_secret : process.env.api_secret
 })
