005e40ffc57530ae891d901ecb205a9a0792e7ea
sushanthraja01 <rajasaidathasushanth2006@gmail.com>
2026-01-08 13:41:03 +0530

    Fix merge conflict in allowedOrigins arraysign in google

--- index.js
+++ index.js
@@ -19,12 +19,9 @@ app.use(passport.initialize());
 const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:5174',
-  'https://hungerspot-backend-dashboard.vercel.app',
-<<<<<<< HEAD
+  'https://hungerspot-backend-dashboard.vercel.app'
   'https://hungerstop-rsds.vercel.app',
-=======
   'https://hungerstop-rsds.vercel.app'
->>>>>>> 8b3271fdddaf804e757eebb5252ccc26f593bc2c
 ];
 
 app.use(cors({
