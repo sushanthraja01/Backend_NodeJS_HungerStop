6480c6dba449edac3457f9132545c8311776f150
sushanthraja01 <rajasaidathasushanth2006@gmail.com>
2026-01-08 13:44:53 +0530

    Fix formatting of allowedOrigins in index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(passport.initialize());
 const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:5174',
-  'https://hungerspot-backend-dashboard.vercel.app'
+  'https://hungerspot-backend-dashboard.vercel.app',
   'https://hungerstop-rsds.vercel.app',
   'https://hungerstop-rsds.vercel.app'
 ];
