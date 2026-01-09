46856390c22c989e25d435c57e9a0498a2d6a491
sushanthraja01 <rajasaidathasushanth2006@gmail.com>
2026-01-09 20:54:57 +0530

    Set trust proxy for Express app

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const cookieParser = require("cookie-parser");
 const passport = require("passport")
 
 app.use(express.json());
+app.set('trust proxy', 1);
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(passport.initialize());
