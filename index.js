d8edcef5ef988a220df3f16d819c87337e0685a9
sushanthraja01 <rajasaidathasushanth2006@gmail.com>
2026-01-10 13:21:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -53,8 +53,8 @@ app.use("/uploads", express.static('uploads'));
 
 const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.BREVO_USER,
     pass: process.env.BREVO_PASS
